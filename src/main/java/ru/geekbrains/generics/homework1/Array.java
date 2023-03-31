a16f26152558b84aa5af51817fb556a5c5cfe18e
afonina <viktoriya.yadyko@mail.ru>
2023-04-01 00:14:14 +0300

    corrected part1 of homework1 has done

--- src/main/java/ru/geekbrains/generics/homework1/Array.java
+++ src/main/java/ru/geekbrains/generics/homework1/Array.java
@@ -5,7 +5,13 @@ public class Array<T> {
     public void swap (T [] elements, int index1, int index2 ) {
         T c = elements [index1];
         elements[index1] = elements [index2];
-        elements [2] = c;
+        elements [index2] = c;
     }
+
+    public void changeArrayToList (T [] elements) {
+
+    }
+
+
 
 }
