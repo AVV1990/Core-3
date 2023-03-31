0af959c0b19d65dda05d1850732bcaa15e1c5218
afonina <viktoriya.yadyko@mail.ru>
2023-04-01 00:04:04 +0300

    part1 of homework1 has done

---
+++ src/main/java/ru/geekbrains/generics/homework1/Array.java
@@ -0,0 +1,11 @@
+package ru.geekbrains.generics.homework1;
+
+public class Array<T> {
+
+    public void swap (T [] elements, int index1, int index2 ) {
+        T c = elements [index1];
+        elements[index1] = elements [index2];
+        elements [2] = c;
+    }
+
+}
--- src/main/java/ru/geekbrains/generics/homework1/MainApp.java
+++ src/main/java/ru/geekbrains/generics/homework1/MainApp.java
@@ -1,7 +1,31 @@
 package ru.geekbrains.generics.homework1;
 
+import java.util.Arrays;
+
 public class MainApp {
     public static void main(String[] args) {
 
+        Integer [] intArr = {1,2,3,4,5,6};
+        String [] stringArr = {"A", "B", "C", "D", "E"};
+
+        System.out.println(Arrays.toString(intArr));
+        System.out.println(Arrays.toString(stringArr));
+
+        Array <Integer> integerArray = new Array<> ();
+        Array <String> stringArray = new Array<> ();
+
+        integerArray.swap(intArr, 1,2);
+        stringArray.swap(stringArr, 1,2);
+
+        System.out.println(Arrays.toString(intArr));
+        System.out.println(Arrays.toString(stringArr));
+
+
+
+
     }
+//    Написать метод, который меняет два элемента массива местами (массив может быть любого ссылочного типа);
+
+
+
 }
